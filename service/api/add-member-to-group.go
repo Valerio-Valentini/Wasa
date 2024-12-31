4170336f25a0d6efe18385d7e803e639d24fdd1a
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2024-12-31 12:40:00 +0100

    Update add-member-to-group.go

--- service/api/add-member-to-group.go
+++ service/api/add-member-to-group.go
@@ -9,15 +9,13 @@ func (rt *_router) addMember(w http.ResponseWriter, r *http.Request, ps httprout
 	w.Header().Set("Content-Type", "application/json")
 	var combo UserChatCombo
 	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil
-	{
+	if err != nil{
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	err := rt.db.AddMember(combo.Chat_id, combo.User_id)
-	if err != nil
-		{
+	if err != nil{
 			w.WriteHeader(http.StatusInternalServerError)
 			ctx.Logger.WithError(err).Error("session: can't create response json")
 			return
