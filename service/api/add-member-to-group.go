bf4f09bbc67e7389f8e9d838d551abbd720b7348
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2024-12-31 12:45:59 +0100

    Update add-member-to-group.go

--- service/api/add-member-to-group.go
+++ service/api/add-member-to-group.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func (rt *_router) addmember(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext){
+func (rt *_router) addmember(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
 	w.Header().Set("Content-Type", "application/json")
 	var combo UserChatCombo
 	err := json.NewDecoder(r.Body).Decode(&user)
