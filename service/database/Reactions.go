70a3acd73f9e42c04daaf76b39df4efd5d7c132a
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2024-12-31 12:16:54 +0100

    test

--- service/database/Reactions.go
+++ service/database/Reactions.go
@@ -1,5 +1,9 @@
 package database
 
+import(
+	"errors"
+)
+
 func (db *appdbimpl) InsertReaction(owner string, reaction string, message int) error {
 	var chat_id int
 	err:= db.c.QueryRow("SELECT chat_id FROM messages WHERE message_id = ? ", message).Scan(&chat_id)
@@ -14,7 +18,7 @@ func (db *appdbimpl) InsertReaction(owner string, reaction string, message int) 
 		return errors.New("User Is Not A Member")
 	}
 
-	_, err:= db.c.Exec("INSERT INTO message_reactions (owner, reaction, message_id) VALUES (?,?,?)", owner, reaction, message)
+	_, err= db.c.Exec("INSERT INTO message_reactions (owner, reaction, message_id) VALUES (?,?,?)", owner, reaction, message)
 	if err != nil {	
 		return err
 	}
@@ -23,12 +27,20 @@ func (db *appdbimpl) InsertReaction(owner string, reaction string, message int) 
 }
 
 func (db *appdbimpl) DeleteReaction(owner string, message int) error {
-	res, err := VerifyUserIsMamberOfChat(owner, chat_id)
+	var chat_id int
+	err:= db.c.QueryRow("SELECT chat_id FROM messages WHERE message_id = ? ", message).Scan(&chat_id)
+	if err != nil {	
+		return err
+	}
+	res, err := db.VerifyUserIsMamberOfChat(owner, chat_id)
 	if err != nil {	
 		return -1, err
 	}
+	if !res{
+		return errors.New("User Is Not A Member")
+	}
 
-	_, err:= db.c.Exec("DELETE FROM message_reactions WHERE (owner = ? AND message = ?)", owner, message)
+	_, err = db.c.Exec("DELETE FROM message_reactions WHERE (owner = ? AND message = ?)", owner, message)
 	if err != nil {	
 		return err
 	}
@@ -37,12 +49,20 @@ func (db *appdbimpl) DeleteReaction(owner string, message int) error {
 }
 
 func (db *appdbimpl) ChangeReaction(owner string, reaction string, message int) error {
-	res, err := VerifyUserIsMamberOfChat(owner, chat_id)
+	var chat_id int
+	err:= db.c.QueryRow("SELECT chat_id FROM messages WHERE message_id = ? ", message).Scan(&chat_id)
+	if err != nil {	
+		return err
+	}
+	res, err := db.VerifyUserIsMamberOfChat(owner, chat_id)
 	if err != nil {	
 		return -1, err
 	}
+	if !res{
+		return errors.New("User Is Not A Member")
+	}
 	
-	_, err:= db.c.Exec("UPDATE message_reactions SET reaction = ? WHERE (owner = ? AND message = ?)", reaction, owner, message)
+	_, err = db.c.Exec("UPDATE message_reactions SET reaction = ? WHERE (owner = ? AND message = ?)", reaction, owner, message)
 	if err != nil {	
 		return err
 	}
