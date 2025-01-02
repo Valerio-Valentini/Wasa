41f7191811b408dfa88e96bb43ae8e7395188bcd
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-01-02 14:05:35 +0100

    Update delete-a-message.go

--- service/api/delete-a-message.go
+++ service/api/delete-a-message.go
@@ -3,7 +3,8 @@ package api
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-)	"encoding/json"
+	"encoding/json"
+)	
 
 func (rt *_router) deleteMessage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
