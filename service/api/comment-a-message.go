f13a4c2faf28a3d477f59bbb3fd5626d565eadaf
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-01-02 14:05:01 +0100

    test

--- service/api/comment-a-message.go
+++ service/api/comment-a-message.go
@@ -9,18 +9,17 @@ import (
 func (rt *_router) commentMessage(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
 	w.Header().Set("Content-Type", "application/json")
 	var reaction Reaction
-	var user string
-	err := json.NewDecoder(r.Body).Decode(&user)
+	err := json.NewDecoder(r.Body).Decode(&reaction)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	err := rt.db.SendMessage(reaction.Owner, reaction.Reaction, reaction.Message_id)
+	id, err := rt.db.InsertReaction(reaction.Owner, reaction.Reaction, reaction.Message_id)
 	if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			ctx.Logger.WithError(err).Error("session: can't create response json")
+			//ctx.Logger.WithError(err).Error("session: can't create response json")
 			return
 		}
-	return nil
+	return 
 }
--- service/api/delete-a-message.go
+++ service/api/delete-a-message.go
@@ -3,22 +3,22 @@ package api
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-)
+)	"encoding/json"
 
 func (rt *_router) deleteMessage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
 	var message Message
-	err := json.NewDecoder(r.Body).Decode(&user)
+	err := json.NewDecoder(r.Body).Decode(&message)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	message_id,err := rt.db.SendMessage(message.Owner, message.Chat_id, message.Message_id)
+	message_id,err := rt.db.DeleteMessage(message.Owner, message.Chat_id, message.Message_id)
 	if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			ctx.Logger.WithError(err).Error("session: can't create response json")
+			//ctx.Logger.WithError(err).Error("session: can't create response json")
 			return
 		}
-	return nil
+	return 
 }
