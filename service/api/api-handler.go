8fd6f0bcdf896d0750ffa01fcf6e29de38ffd8be
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-01-02 13:48:40 +0100

    test

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -8,28 +8,28 @@ import (
 func (rt *_router) Handler() http.Handler {
 	// USER
 	rt.router.PUT("/users/:user_id", rt.putNewUserNickname) //ok
-	rt.router.PUT("/users/:user_id/photo", rt.wrap(rt.putNewUserPhoto)) //ok
+	rt.router.PUT("/users/:user_id/photo", rt.putNewUserPhoto) //ok
 	//SEARCH
-	rt.router.GET("/users", rt.wrap(rt.searchUser)) //ok
+	rt.router.GET("/users", rt.searchUser) //ok
 	//CHAT
-	rt.router.GET("/users/:user_id/chats", rt.wrap(rt.getAllChats)) //ok
-	rt.router.GET("/users/:user_id/chats/:chat_id", rt.wrap(rt.getMessagesFromChat)) //ok
+	rt.router.GET("/users/:user_id/chats", rt.getAllChats) //ok
+	rt.router.GET("/users/:user_id/chats/:chat_id", rt.getMessagesFromChat) //ok
 	//GROUP
-	rt.router.PUT("/users/:user_id/chats/:chat_id/photo", rt.wrap(rt.liveness)) //ok
-	rt.router.PUT("/users/:user_id/chats/:chat_id/members/:member_id", rt.wrap(rt.addMember)) //ok
-	rt.router.PUT("/users/:user_id/chats/:chat_id", rt.wrap(rt.liveness)) 
-	rt.router.DELETE("/users/:user_id/chats/:chat_id", rt.wrap(rt.leaveChat)) //ok
+	rt.router.PUT("/users/:user_id/chats/:chat_id/photo", rt.liveness) //ok
+	rt.router.PUT("/users/:user_id/chats/:chat_id/members/:member_id", rt.addMember) //ok
+	rt.router.PUT("/users/:user_id/chats/:chat_id", rt.liveness) 
+	rt.router.DELETE("/users/:user_id/chats/:chat_id", rt.leaveChat) //ok
 	//MESSAGE
-	rt.router.POST("/users/:user_id/chats/:chat_id/message/:message_id", rt.wrap(rt.sendmessage)) //ok
-	rt.router.DELETE("/users/:user_id/chats/:chat_id/message/:message_id", rt.wrap(rt.deleteMessage)) //ok
-	rt.router.PUT("/users/:user_id/chats/:chat_id/message/:message_id/reaction/:reaction_id", rt.wrap(rt.commentMessage)) //ok
-	rt.router.DELETE("/users/:user_id/chats/:chat_id/message/:message_id/reaction/:reaction_id", rt.wrap(rt.deleteComment)) //ok
-	rt.router.POST("/users/:user_id/chats/:chat_id/message/:message_id/forwarded/:message_id", rt.wrap(rt.ForwardMessage)) //ok
+	rt.router.POST("/users/:user_id/chats/:chat_id/message/:message_id", rt.sendmessage) //ok
+	rt.router.DELETE("/users/:user_id/chats/:chat_id/message/:message_id", rt.deleteMessage) //ok
+	rt.router.PUT("/users/:user_id/chats/:chat_id/message/:message_id/reaction/:reaction_id", rt.commentMessage) //ok
+	rt.router.DELETE("/users/:user_id/chats/:chat_id/message/:message_id/reaction/:reaction_id", rt.deleteComment) //ok
+	rt.router.POST("/users/:user_id/chats/:chat_id/message/:message_id/forwarded/:message_id", rt.ForwardMessage) //ok
 	//MEDIA
-	rt.router.POST("/users/:user_id/chats/:chat_id/media/:media_id", rt.wrap(rt.sendMedia)) //ok
-	rt.router.DELETE("/users/:user_id/chats/:chat_id/media/:media_id", rt.wrap(rt.deleteMedia)) //ok
+	rt.router.POST("/users/:user_id/chats/:chat_id/media/:media_id", rt.sendMedia) //ok
+	rt.router.DELETE("/users/:user_id/chats/:chat_id/media/:media_id", rt.deleteMedia) //ok
 	//LOGIN
-	rt.router.POST("/session", rt.wrap(rt.session)) //ok
+	rt.router.POST("/session", rt.session) //ok
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness) //ok
 
