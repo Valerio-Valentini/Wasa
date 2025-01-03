e8162ffb07a3003b45c8132a31d6841a1ad7b13c
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-01-03 14:15:03 +0100

    test

--- service/api/put-new-group-name.go
+++ service/api/put-new-group-name.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 )
 
-func (rt *_router) putNewUserNickname(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (rt *_router) putNewGroupName(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
 	var values NameChat
 	err := json.NewDecoder(r.Body).Decode(&name)
@@ -15,7 +15,7 @@ func (rt *_router) putNewUserNickname(w http.ResponseWriter, r *http.Request, ps
 		return
 	}
 
-	err = rt.db.UpdateSetGroupName(values.User_id, values.Chat_id, values.Chat_name)
+	err = rt.db.SetGroupName(values.User_id, values.Chat_id, values.Chat_name)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		//ctx.Logger.WithError(err).Error("session: can't create response json")
