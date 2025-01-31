7a4517a973ae900f5be5be8fa50c8df0f2f82473
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-01-31 18:47:33 +0100

    test

--- service/database/Utility.go
+++ service/database/Utility.go
@@ -16,7 +16,7 @@ func (db *appdbimpl) VerifyUser(username string) (bool, error) {
 	return true, nil
 }
 
-func (db *appdbimpl) VerifyUserIsMamberOfChat(user_id string, chat_id int) (bool, error) {
+func (db *appdbimpl) VerifyUserIsMamberOfChat(user_id string, chat_id string) (bool, error) {
 	var presence int
 	err := db.c.QueryRow("SELECT 1 FROM chat_members WHERE user_id = ? AND chat_id = ? LIMIT 1 ", user_id, chat_id).Scan(&presence)
 	if err != nil {
