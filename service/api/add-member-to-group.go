617d6ab0818b34995b656c92eecdf1c01c6af8e4
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-02-01 12:54:07 +0100

    Update add-member-to-group.go

--- service/api/add-member-to-group.go
+++ service/api/add-member-to-group.go
@@ -3,12 +3,11 @@ package api
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"encoding/json"
 )
 
-func (rt *_router) addMember(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (rt *_router) addAMember(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
-	err := rt.db.addAMember(ps.ByName("chat_id"), ps.ByName("member_id"))
+	err := rt.db.addMember(ps.ByName("chat_id"), ps.ByName("member_id"))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
