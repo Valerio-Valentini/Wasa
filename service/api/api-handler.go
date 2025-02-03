c6d2a1865994d3ebdc038ef80ad9cdba00f1f121
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-02-03 11:29:12 +0100

    test

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -7,29 +7,29 @@ import (
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 	// USER
-	rt.router.PUT("/users/:user_id", rt.putNewUserNickname)    // ok
-	rt.router.PUT("/users/:user_id/photo", rt.putNewUserPhoto) // notok
+	rt.router.PUT("/users/:user_id", rt.putNewUserNickname)    // ok +
+	rt.router.PUT("/users/:user_id/photo", rt.putNewUserPhoto) // notok +
 	// SEARCH
-	rt.router.GET("/users", rt.searchUser) // ok
+	rt.router.GET("/users", rt.searchUser) // ok +
 	// CHAT
-	rt.router.GET("/users/:user_id/chats", rt.getAllChats)                  // ok
-	rt.router.GET("/users/:user_id/chats/:chat_id", rt.getMessagesFromChat) // ok
+	rt.router.GET("/users/:user_id/chats", rt.getAllChats)                  // ok +
+	rt.router.GET("/users/:user_id/chats/:chat_id", rt.getMessagesFromChat) // ok +
 	// GROUP
-	rt.router.PUT("/chats/:chat_id/photo", rt.putNewGroupPhoto)        // notok
-	rt.router.PUT("/chats/:chat_id/members/:member_id", rt.addAMember) // ok
-	rt.router.PUT("/chats/:chat_id", rt.putNewGroupName)               // ok
-	rt.router.DELETE("/chats/:chat_id/members/:member_id", rt.leaveChat)                  // ok
+	rt.router.PUT("/chats/:chat_id/photo", rt.putNewGroupPhoto)        // notok +
+	rt.router.PUT("/chats/:chat_id/members/:member_id", rt.addAMember) // ok +
+	rt.router.PUT("/chats/:chat_id", rt.putNewGroupName)               // ok +
+	rt.router.DELETE("/chats/:chat_id/members/:member_id", rt.leaveChat)                  // ok +
 	// MESSAGE
-	rt.router.POST("/chats/:chat_id/messages", rt.sendmessage)                           // ok
-	rt.router.DELETE("/chats/:chat_id/messages/:message_id", rt.deleteMessage)                       // ok
-	rt.router.PUT("/chats/:chat_id/messages/:message_id/reactions/:reaction_id", rt.commentMessage)   // ok
-	rt.router.DELETE("/chats/:chat_id/messages/:message_id/reactions/:reaction_id", rt.deleteComment) // ok
-	rt.router.POST("/chats/:chat_id/forwarded/:message_id", rt.forwardMessage)  // ok
+	rt.router.POST("/chats/:chat_id/messages", rt.sendmessage)                   // ok +
+	rt.router.DELETE("/chats/:chat_id/messages/:message_id", rt.deleteMessage)                       // ok +
+	rt.router.PUT("/chats/:chat_id/messages/:message_id/reactions/:reaction_id", rt.commentMessage)   // ok +
+	rt.router.DELETE("/chats/:chat_id/messages/:message_id/reactions/:reaction_id", rt.deleteComment) // ok +
+	rt.router.POST("/chats/:chat_id/forwarded/:message_id", rt.forwardMessage)  // ok +
 	// MEDIA
-	rt.router.POST("/chats/:chat_id/media/:media_id", rt.sendMedia)     // notok
-	rt.router.DELETE("/chats/:chat_id/media/:media_id", rt.deleteMedia) // notok
+	rt.router.POST("/chats/:chat_id/media/:media_id", rt.sendMedia)     // notok -
+	rt.router.DELETE("/chats/:chat_id/media/:media_id", rt.deleteMedia) // notok -
 	// LOGIN
-	rt.router.POST("/session", rt.session) // ok
+	rt.router.POST("/session", rt.session) // ok +
 
 	return rt.router
 }
---
+++ webui/src/views/Page.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="contaienr-fluid">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">WasaText</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="#">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">LogOut</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">NewGroup</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <nav class="navbar bg-body-tertiary">
+                                    <div class="container-fluid">
+                                        <form class="d-flex" role="search">
+                                            <input class="form-control me-2" type="search" placeholder="@username" aria-label="Search">
+                                            <button class="btn btn-outline-success" type="submit">Search</button>
+                                        </form>
+                                    </div>
+                                </nav>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                        The current link item
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">link item</a>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card" id="spc">
+                                
+                                <div class="card-body">
+                                    <p class="card-text">testo</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="input-group" id="txt">
+                                <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+                                <span class="input-group-text" id="btn">Invia</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    #spc {
+        height: 730px;
+        margin-top: 10px;
+        margin-left: -25px;
+    }
+    #txt {
+        margin-left: -25px;
+        margin-top: 5px;
+        height: 75px;
+        width: 865px;
+    }
+    #btn {
+        background-color: rgb(13, 110, 253);
+        color: white;
+    }
+</style>
