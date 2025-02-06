3840355af233f370d6110a7227204d00690ac37c
Valerio-Valentini <valentini.2046860@studenti.uniroma1.it>
2025-02-06 15:59:27 +0100

    Update createNewChat.go

--- service/api/createNewChat.go
+++ service/api/createNewChat.go
@@ -6,10 +6,5 @@ import (
 )
 
 func (rt *_router) createNewChat(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	w.Header().Set("Content-Type", "application/json")
-	err := rt.db.NewChat(ps.ByName("chat_group"), ps.ByName("photo_id"), ps.ByName("chat_name"))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
+	// Da finire
 }
